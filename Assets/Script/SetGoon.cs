8e5903fd9ae4fcb411345563dcafd24c9b0c24f6
ofgonen <ofgonen@gmail.com>
2022-06-22 13:09:43 +0300

    Implement SetGoon
    - Create shared asset

---
+++ Assets/Script/SetGoon.cs
@@ -0,0 +1,11 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "set_stage_goon", menuName = "FF/Data/Set/Stage Goons" ) ]
+public class SetGoon : RuntimeSet< int, Goon >
+{
+}
