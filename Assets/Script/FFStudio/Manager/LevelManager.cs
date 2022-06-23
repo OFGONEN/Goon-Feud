d3d8231234e2f74b904ffb018925a2c6a23d5148
ofgonen <ofgonen@gmail.com>
2022-06-23 13:09:45 +0300

    Implement LevelManager

--- Assets/Script/FFStudio/Manager/LevelManager.cs
+++ Assets/Script/FFStudio/Manager/LevelManager.cs
@@ -2,18 +2,28 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
 
 namespace FFStudio
 {
     public class LevelManager : MonoBehaviour
     {
 #region Fields
-        [ Header( "Fired Events" ) ]
-        public GameEvent levelFailedEvent;
-        public GameEvent levelCompleted;
+    [ Title( "Fired Events" ) ]
+        [ SerializeField ] GameEvent event_level_completed;
+        [ SerializeField ] GameEvent event_level_failed;
+        [ SerializeField ] GameEvent event_stage_continue;
+        [ SerializeField ] GameEvent event_stage_end;
+		[ SerializeField ] GameEvent event_ui_answer_submit;
+		[ SerializeField ] GameEvent event_ui_question_appear;
+        [ SerializeField ] GameEvent event_ui_question_disappear;
 
-        [ Header( "Level Releated" ) ]
-        public SharedFloatNotifier levelProgress;
+    [ Title( "Shared Variables" ) ]
+        [ SerializeField ] SetGoon set_goon;
+        [ SerializeField ] SharedIntNotifier notif_ui_question_index;
+        [ SerializeField ] SharedIntNotifier notif_player_stage_index; 
+        [ SerializeField ] SharedFloatNotifier levelProgress;
+        [ SerializeField ] SharedReferenceNotifier notif_player_transform;
 #endregion
 
 #region UnityAPI
@@ -44,9 +54,63 @@ namespace FFStudio
         {
 
         }
+
+        // Info: Seriliazed Call of value change on the notif_goon_path_count
+        public void OnGoonPathingCount( int value )
+        {
+			var questionCount        = CurrentLevelData.Instance.levelData.stage_data[ notif_player_stage_index.sharedValue ].stage_question.Length;
+			var allQuestionsAnswered = questionCount <= notif_ui_question_index.sharedValue;
+
+            // All goons are stopped pathing
+            if( value == 0 ) 
+            {
+                // There is no other question to answer
+                // So one goon needs to go and kill the Player
+                if( allQuestionsAnswered )
+					KillPlayerWithGoon();
+				else
+				    event_ui_question_appear.Raise(); // There is question to answer
+            }
+		}
+        
+        // Info: Seriliazed Call of value change on the notif_ui_answer_submit_count
+        public void OnAnswerSubmit( int value )
+        {
+            // All Questions are answered or All alive goons are submited with an answer since there is less goon than answer count
+            if( value == ExtensionMethods.ANSWER_COUNT || value == set_goon.itemDictionary.Count )
+				event_ui_question_disappear.Raise();
+		}
+
+        // Info: Seriliazed Call of value change on the notif_player_stage_index
+        public void OnPlayerStageIndexChange( int value )
+        {
+            if( value == CurrentLevelData.Instance.levelData.stage_count )
+				event_level_completed.Raise();
+		}
 #endregion
 
 #region Implementation
+        void KillPlayerWithGoon()
+        {
+			// Find closest goon 
+			var playerPosition = ( notif_player_transform.SharedValue as Transform ).position;
+
+			Goon closestGoon = null;
+			float closestDistance = float.MaxValue;
+
+            foreach( var goon in set_goon.itemDictionary.Values )
+            {
+				var goonDistance = Vector3.Distance( goon.transform.position, playerPosition );
+
+                if( goonDistance < closestDistance )
+                {
+					closestDistance = goonDistance;
+					closestGoon     = goon;
+				}
+			}
+
+			closestGoon.PathToPlayer();
+		}
 #endregion
     }
 }
