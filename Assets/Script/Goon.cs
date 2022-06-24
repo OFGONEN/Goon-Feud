3d2d01463c4616a76682f65f23756f87ba2f8a40
ofgonen <ofgonen@gmail.com>
2022-06-24 15:31:25 +0300

    Goon: Change materials on death

--- Assets/Script/Goon.cs
+++ Assets/Script/Goon.cs
@@ -13,6 +13,7 @@ public class Goon : MonoBehaviour
     [ LabelText( "ID of the Stage Goon belongs to"), BoxGroup( "Setup" ), SerializeField ] int goon_stage_id;
     [ LabelText( "ID of the Goon in current Stage" ), BoxGroup( "Setup" ), SerializeField ] int goon_id;
     [ LabelText( "Health Count" ), BoxGroup( "Setup" ), SerializeField ] int goon_health;
+    [ BoxGroup( "Setup" ), SerializeField ] Material[] goon_material_killed;
 
     [ BoxGroup( "Shared Variables" ), SerializeField ] SharedIntNotifier notif_player_stage_index;
     [ BoxGroup( "Shared Variables" ), SerializeField ] SetGoon set_stage_goon;
@@ -21,6 +22,7 @@ public class Goon : MonoBehaviour
     [ BoxGroup( "Component" ), SerializeField ] Animator goon_animator;
     [ BoxGroup( "Component" ), SerializeField ] GoonMovement goon_movement;
     [ BoxGroup( "Component" ), SerializeField ] GoonLine goon_line;
+    [ BoxGroup( "Component" ), SerializeField ] SkinnedMeshRenderer goon_renderer;
 
     RecycledTween recycledTween = new RecycledTween();
 
@@ -108,6 +110,8 @@ public class Goon : MonoBehaviour
     {
 		goon_animator.SetTrigger( "hit" );
 		recycledTween.Recycle( DOVirtual.DelayedCall( GameSettings.Instance.goon_hit_delay, event_player_killed.Raise ) );
+
+		goon_renderer.sharedMaterials = goon_material_killed; 
 	}
 
 	void EmptyDelegates()
