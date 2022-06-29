62ad802f06b40788516c8cf7df0b07181d4b5190
ofgonen <ofgonen@gmail.com>
2022-06-29 19:59:52 +0300

    PoolUIHitPoint: Attach ui hit point to current stage goons

--- Assets/Script/PoolUIHitPoint.cs
+++ Assets/Script/PoolUIHitPoint.cs
@@ -4,8 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using Sirenix.OdinInspector;
 
 [ CreateAssetMenu( fileName = "pool_ui_hitPoint", menuName = "FF/Data/Pool/UI Hit Point" ) ]
 public class PoolUIHitPoint : ComponentPool< UIHitPoint >
 {
+    [ BoxGroup( "Setup" ), SerializeField ] SetGoon set_goon;
+
+    public void AttachToGoons()
+    {
+        foreach( var goon in set_goon.itemDictionary.Values )
+        {
+			GetEntity().AttachToGoon( goon );
+		}
+    }
 }
