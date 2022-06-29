7b1e87450b12d4aa4918d295b4ebfc6878cc2735
ofgonen <ofgonen@gmail.com>
2022-06-29 19:58:36 +0300

    UIHitPoint: Attach to a Goon

--- Assets/Script/UIHitPoint.cs
+++ Assets/Script/UIHitPoint.cs
@@ -3,10 +3,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using FFStudio;
+using Sirenix.OdinInspector;
 
 public class UIHitPoint : MonoBehaviour
 {
 #region Fields
+    [ BoxGroup( "Setup" ), SerializeField ] Image ui_image;
+    [ BoxGroup( "Setup" ), SerializeField ] SharedReferenceNotifier notif_camera_transform;
+
+    Goon goon_current;
+    Camera camera_main;
+	RectTransform ui_rectTransform;
 #endregion
 
 #region Properties
@@ -16,6 +25,18 @@ public class UIHitPoint : MonoBehaviour
 #endregion
 
 #region API
+    public void AttachToGoon( Goon goon )
+    {
+		gameObject.SetActive( true );
+		ui_rectTransform = ui_image.rectTransform;
+
+		ui_image.color   = GameSettings.Instance.hitpoint_color_default;
+		ui_image.enabled = true;
+		goon_current     = goon;
+
+		camera_main = ( notif_camera_transform.SharedValue as Transform ).GetComponent< Camera >();
+		transform.position = camera_main.WorldToScreenPoint( goon_current.GoonPosition );
+	}
 #endregion
 
 #region Implementation
