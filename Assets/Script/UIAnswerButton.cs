5a7908e3c4b8771be68d66af2dd21c88d9a26125
ofgonen <ofgonen@gmail.com>
2022-06-30 12:31:10 +0300

    UIAnswerButton: Caching and Returning answer

--- Assets/Script/UIAnswerButton.cs
+++ Assets/Script/UIAnswerButton.cs
@@ -19,10 +19,17 @@ public class UIAnswerButton : MonoBehaviour
     [ BoxGroup( "UI Elements" ), SerializeField ] TextMeshProUGUI ui_text_button; // child game-object of ui_image_button
 
     AnswerData answer_data;
-    Vector3 ui_image_button_position;
+    Vector3 ui_image_button_startPosition;
     Vector3 ui_image_button_velocity;
 
-    UnityMessage onFingerUp;
+	UIHitPoint ui_hitPoint_current;
+	float answer_submit_distance;
+
+	bool answer_cached;
+
+
+
+	UnityMessage onFingerUp;
     UnityMessage onUpdate;
 #endregion
 
@@ -38,7 +45,8 @@ public class UIAnswerButton : MonoBehaviour
 
     private void Start()
     {
-		ui_image_button_position = ui_image_button.rectTransform.position;
+		answer_submit_distance   = Screen.height * GameSettings.Instance.answer_submit_distance / 100f;
+		ui_image_button_startPosition = ui_image_button.rectTransform.position;
 	}
 
     private void Update()
@@ -50,15 +58,21 @@ public class UIAnswerButton : MonoBehaviour
 #region API
     public void OnQuestionAppear()
     {
-		answer_data = CurrentLevelData.Instance.CurrentQuestion.question_answer[ button_id ];
+		answer_data         = CurrentLevelData.Instance.CurrentQuestion.question_answer[ button_id ];
+		ui_hitPoint_current = null;
 
 		ui_image_button.gameObject.SetActive( true );
 		ui_image_button.rectTransform.localScale = Vector3.one;
-		ui_image_button.rectTransform.position   = ui_image_button_position;
+		ui_image_button.rectTransform.position   = ui_image_button_startPosition;
 
 		ui_text_button.text = answer_data.answer;
 
 		ui_image_button_return.gameObject.SetActive( false );
+
+		ui_image_button.raycastTarget        = true;
+		ui_image_button_return.raycastTarget = true;
+
+		answer_cached = false;
 	}
 
     public void OnQuestionDisappear()
@@ -69,13 +83,23 @@ public class UIAnswerButton : MonoBehaviour
 
     public void AnswerTransparent()
     {
-		ui_image_button.SetAlpha( GameSettings.Instance.answer_transparent_value );
+		ui_image_button.raycastTarget        = false;
+		ui_image_button_return.raycastTarget = false;
+
 		ui_image_button_return.SetAlpha( GameSettings.Instance.answer_transparent_value );
-		ui_text_button.SetAlpha( GameSettings.Instance.answer_transparent_value );
+
+        if( !answer_cached )
+        {
+		    ui_image_button.SetAlpha( GameSettings.Instance.answer_transparent_value );
+            ui_text_button.SetAlpha( GameSettings.Instance.answer_transparent_value );
+        }
 	}
 
     public void AnswerOpaque()
     {
+		ui_image_button.raycastTarget        = !answer_cached;
+		ui_image_button_return.raycastTarget = answer_cached;
+
 		ui_image_button.SetAlpha( 1 );
 		ui_image_button_return.SetAlpha( 1 );
 		ui_text_button.SetAlpha( 1 );
@@ -84,12 +108,28 @@ public class UIAnswerButton : MonoBehaviour
     public void OnButtonClick()
     {
 		ui_image_button_velocity = Vector3.zero;
+		ui_hitPoint_current      = null;
 
 		onFingerUp = FingerUp;
 		onUpdate   = OnUpdate;
 
         // Since event_quesiton_transparent event is called on a answer button click
 		AnswerOpaque();
+	}
+
+    public void OnReturnButtonClick()
+    {
+		ui_hitPoint_current.OnAnswerClear();
+		ui_hitPoint_current = null;
+
+		answer_cached = false;
+
+		ui_image_button_return.gameObject.SetActive( false );
+
+		ui_image_button.gameObject.SetActive( true );
+		ui_image_button.rectTransform.localScale = Vector3.one;
+		ui_image_button.rectTransform.position   = ui_image_button_startPosition;
+		ui_image_button.raycastTarget            = true;
 	}
 
     public void OnFingerUp()
@@ -104,7 +144,20 @@ public class UIAnswerButton : MonoBehaviour
 		onUpdate   = ExtensionMethods.EmptyMethod;
 		onFingerUp = ExtensionMethods.EmptyMethod;
 
-		ui_image_button.rectTransform.position   = ui_image_button_position;
+        if( ui_hitPoint_current )
+        {
+			ui_image_button.rectTransform.position   = ui_hitPoint_current.transform.position;
+			ui_image_button.rectTransform.localScale = GameSettings.Instance.answer_submit_scale * Vector3.one;
+            
+		    ui_image_button_return.gameObject.SetActive( true );
+			ui_image_button.raycastTarget = false;
+
+			answer_cached = true;
+
+			ui_hitPoint_current.OnAnswerCache( answer_data.answer_value );
+		}
+        else
+		    ui_image_button.rectTransform.position = ui_image_button_startPosition;
     }
     
     void OnUpdate()
@@ -113,6 +166,31 @@ public class UIAnswerButton : MonoBehaviour
 		var targetPosition = Input.mousePosition;
 
 		ui_image_button.transform.position = Vector3.SmoothDamp( position, targetPosition, ref ui_image_button_velocity, GameSettings.Instance.answer_drag_smoothTime );
+
+		UIHitPoint closestUIHitPoint = null;
+		float closestDistance = float.MaxValue;
+
+        foreach( var hitPoint in set_ui_hitPoint.itemList )
+        {
+			var distance = Vector3.Distance( hitPoint.transform.position, ui_image_button.transform.position );
+
+            if( distance < answer_submit_distance && distance < closestDistance )
+				closestUIHitPoint = hitPoint;
+		}
+
+        if( closestUIHitPoint )
+        {
+            if( ui_hitPoint_current )
+				ui_hitPoint_current.OnDefault();
+
+			ui_hitPoint_current = closestUIHitPoint;
+			ui_hitPoint_current.OnSelected();
+		}
+        else if( ui_hitPoint_current )
+        {
+			ui_hitPoint_current.OnDefault();
+			ui_hitPoint_current = null;
+		}
 	}
 #endregion
 
